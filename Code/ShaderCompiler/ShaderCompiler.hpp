ba6573d07574363b7069a9c50c313e460f4a6e29
Manuel Maier <mjmaier@gmx.de>
2016-08-04 20:01:23 +0200

    Link ShaderCompiler into Application

--- Code/ShaderCompiler/ShaderCompiler.hpp
+++ Code/ShaderCompiler/ShaderCompiler.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#if defined(SHADER_COMPILER_EXPORT_DLL)
+  #define SHADER_COMPILER_API __declspec(dllexport)
+#else
+  #define SHADER_COMPILER_API __declspec(dllimport)
+#endif
+
 #include <Core/Allocator.hpp>
 
 #include <Cfg/Cfg.hpp>
@@ -15,7 +21,7 @@ enum class glsl_shader_stage
   Compute,
 };
 
-class glsl_shader
+class SHADER_COMPILER_API glsl_shader
 {
 public:
   dynamic_array<char> EntryPoint{};
@@ -30,20 +36,25 @@ struct shader_compiler_context
 {
 };
 
+SHADER_COMPILER_API
 shader_compiler_context*
 CreateShaderCompilerContext(allocator_interface* Allocator);
 
+SHADER_COMPILER_API
 void
 DestroyShaderCompilerContext(allocator_interface* Allocator, shader_compiler_context* Context);
 
+SHADER_COMPILER_API
 bool
 CompileCfgToGlsl(shader_compiler_context* Context, cfg_node const* ShaderRoot,
                  glsl_shader* GlslShader);
 
+SHADER_COMPILER_API
 bool
 CompileGlslToSpv(shader_compiler_context* Context, glsl_shader const* GlslShader,
                  dynamic_array<uint32>* SpvByteCode);
 
+SHADER_COMPILER_API
 bool
 CompileCfgToGlslAndSpv(shader_compiler_context* Context, cfg_node const* ShaderRoot,
                        glsl_shader* GlslShader, dynamic_array<uint32>* SpvByteCode);
