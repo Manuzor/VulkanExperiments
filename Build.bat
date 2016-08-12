a971975cec01ec4e47c04819046ca13880a595b1
Manuel Maier <mjmaier@gmx.de>
2016-08-12 11:14:13 +0200

    Make Build.bat location independent

--- Build.bat
+++ Build.bat
@@ -1,4 +1,6 @@
-@"%~dp0Utilities\FASTBuild\FBuild.exe" %*
+@pushd "%~dp0Workspace"
+@"%~dp0Utilities\FASTBuild\FBuild.exe" -config "%~dp0fbuild.bff" %*
+@popd
 
 @rem Only pause if run from explorer via double-click.
 @if not "%0" == "%~0" @pause
